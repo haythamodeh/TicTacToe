9c0d9864cf002b6945a0a3045c27b4963ec564b5
haytham odeh <haythamlion@outlook.com>
2023-05-13 15:56:40 -0700

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Haytham Odeh on 5/13/23.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
