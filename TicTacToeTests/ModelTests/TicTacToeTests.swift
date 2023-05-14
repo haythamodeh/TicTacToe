74271aed18f250f2cdf3e4a77d12f962de8e9025
haytham odeh <haythamlion@outlook.com>
2023-05-13 17:21:13 -0700

    Build desired functionality in the Model, adding functionality test,  if player X puts their token in the middle square, then player O is not allowed to replace it with their token or erase the X token

--- TicTacToeTests/ModelTests/TicTacToeTests.swift
+++ TicTacToeTests/ModelTests/TicTacToeTests.swift
@@ -9,6 +9,7 @@
 import XCTest
 
 final class TicTacToeTests: XCTestCase {
+    
     func test_initialValue_NineCells() {
         // Arrange
         let ticModel = TicModel()
@@ -39,5 +40,33 @@ final class TicTacToeTests: XCTestCase {
 
         // Assert
         XCTAssertTrue(ticModel.grid.contains { $0 == cell.x })
+    }
+
+    func test_setCellTwice_ignoreSecond() {
+        // Arrange
+        var ticModel = TicModel()
+
+        // Act
+        ticModel.setCell(n: 3, c: .x)
+        ticModel.setCell(n: 3, c: .o)
+
+        // Assert
+        XCTAssertTrue(ticModel.grid[3] == cell.x)
+        XCTAssertEqual((ticModel.grid.filter { $0 == cell.x }.count), 1)
+        XCTAssertEqual((ticModel.grid.filter { $0 == cell.b }.count), 8)
+    }
+
+    func test_setCellBlank_ignored() {
+        // Arrange
+        var ticModel = TicModel()
+
+        // Act
+        ticModel.setCell(n: 3, c: .x)
+        ticModel.setCell(n: 3, c: .b)
+
+        // Assert
+        XCTAssertTrue(ticModel.grid[3] == cell.x)
+        XCTAssertEqual((ticModel.grid.filter { $0 == cell.x }.count), 1)
+        XCTAssertEqual((ticModel.grid.filter { $0 == cell.b }.count), 8)
     }
 }
